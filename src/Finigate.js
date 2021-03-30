e9d76fa7ab9ee2d3df4891be2c1d8b2f431cbb3f
lilipan56 <lilipan56@gmail.com>
2021-03-30 11:17:42 +0200

    add animation for next, prev and buttons

---
+++ src/Finigate.js
@@ -0,0 +1,51 @@
+window.onload = function () {
+    let container = document.getElementById('container');
+    let list = document.getElementById('list');
+    let buttons = document.getElementById('buttons').getElementsByTagName('span');
+    let prev = document.getElementById('prev');
+    let next = document.getElementById('next');
+    let index = 1;
+
+    function showButton() {
+        for (let i=0; i < buttons.length; i++){
+            if (buttons[i].className === 'on'){
+                buttons[i].className = '';
+                break;
+            }
+        }
+        buttons[index - 1].className = 'on';
+    }
+
+    function animate(offset) {
+        let newLeft = parseInt(list.style.left) + offset;
+        list.style.left = `${newLeft}px`;
+        if(newLeft > -1150){
+            list.style.left = `${-5750}px`;
+        }
+        if(newLeft < -5750){
+            list.style.left = `${-1150}px`;
+        }
+    }
+
+    next.onclick = function () {
+        if (index === 5){
+            index = 1;
+        }else{
+            index += 1;
+        }
+        
+        showButton();
+        animate(-1150)
+    }
+
+    prev.onclick = function () {
+        if (index === 1){
+            index = 5;
+        }else{
+            index -= 1;
+        }
+        
+        showButton();
+        animate(1150)
+    }
+}
